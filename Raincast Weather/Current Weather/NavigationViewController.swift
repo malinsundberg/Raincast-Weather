dcd13d040abe79575a8f6d168879c15e6335943d
Malin Sundberg <malinsundberg@users.noreply.github.com>
2020-03-11 00:51:01 -0700

    Feature/project updates (#1)
    * Changed title text color
    
    * Updated swift version to 5.0
    
    * Removed unneeded code
    
    * Made status bar content light

---
+++ Raincast Weather/Current Weather/NavigationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationViewController.swift
+//  Raincast Weather
+//
+//  Created by Malin Sundberg on 2020-03-10.
+//  Copyright © 2020 Malin Sundberg. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        .lightContent
+    }
+}
--- Raincast Weather/Shared/AppDelegate.swift
+++ Raincast Weather/Shared/AppDelegate.swift
@@ -13,6 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let scrollEdgeAppearance = UINavigationBarAppearance()
+        scrollEdgeAppearance.configureWithOpaqueBackground()
+        scrollEdgeAppearance.backgroundColor = UIColor(named: "NavigationBarColor")
+        scrollEdgeAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        UINavigationBar.appearance(whenContainedInInstancesOf: [UINavigationController.self]).standardAppearance = scrollEdgeAppearance
+        UINavigationBar.appearance(whenContainedInInstancesOf: [UINavigationController.self]).scrollEdgeAppearance = scrollEdgeAppearance
+        UINavigationBar.appearance(whenContainedInInstancesOf: [UINavigationController.self]).compactAppearance = scrollEdgeAppearance
+        
         return true
     }
 }
